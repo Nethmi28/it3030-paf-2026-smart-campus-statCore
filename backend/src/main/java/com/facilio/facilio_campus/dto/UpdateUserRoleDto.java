4d001673c006b2e8ff1bae8d0afed9c92e4f7860
PoornaviSina <hansikalakshani2006@gmail.com>
2026-04-15 18:13:03 +0530

    Added assign roles part for admin

---
+++ backend/src/main/java/com/facilio/facilio_campus/dto/UpdateUserRoleDto.java
@@ -0,0 +1,16 @@
+package com.facilio.facilio_campus.dto;
+
+public class UpdateUserRoleDto {
+
+    private String role;
+
+    public UpdateUserRoleDto() {}
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
